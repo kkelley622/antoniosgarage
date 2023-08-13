248ecea29446e3644a4a2c1bfdb4a763eff143bf
Kevin Kelley <kkelley622@gmail.com>
2023-08-13 17:31:40 -0500

    Added styling for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <ul>
-      <li><Link to='/'>Casa</Link></li>
-      <li><Link to='/sobre'>Sobre Nosotros</Link></li>
-      <li><Link to='/servicios'>Servicios</Link></li>
-      <li><Link to='/contacta'>Contacta con Nosotros</Link></li>
-    </ul>
+    <nav>
+      <ul>
+        <li><Link to='/'>Casa</Link></li>
+        <li><Link to='/sobre'>Sobre Nosotros</Link></li>
+        <li><Link to='/servicios'>Servicios</Link></li>
+        <li><Link to='/contacta'>Contacta con Nosotros</Link></li>
+      </ul>
+    </nav>
   )
 }
 
