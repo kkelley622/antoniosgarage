9e736a57c4ea7d1480b1e965c3d5028c00e56ce4
Kevin Kelley <kkelley622@gmail.com>
2023-08-18 16:34:42 -0500

    Trying to implement google maps in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { GoogleMap, useLoadScript } from '@react-google-maps/api'
 
 const Contact = () => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.AIzaSyBEJ1MD9CLuELvuu_PPfZxu6cZhqtxGMUU,
+  });
+  const center = useMemo(() => ({lat: 18.52043, lng: 73.856743}), []);
+
   return (
-    <div>Contact</div>
-  )
-}
+    <div className='location'>
+    {!isLoaded ? (
+      <h1>Loading...</h1>
+    ) : (
+      <GoogleMap
+        mapContainerClassName="map-container"
+        center={center}
+        zoom={10}
+      />
+    )}
+  </div>
+  );
+};
 
 export default Contact
