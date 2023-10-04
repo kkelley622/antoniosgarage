c5321be801c8f0cb8b7dd4a6d7012fa18b13ec70
Kevin Kelley <kkelley622@gmail.com>
2023-10-04 16:20:57 -0500

    Added examples on Services page.

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div>
+      <h1>Services We Provide</h1>
+      <p>At Antonio's Garage we try to meet all of your mechanical needs. We specialize in the
+        following car makes:
+      </p>
+      <ul>
+        <li>Toyota</li>
+        <li>Nissan</li>
+        <li>Chevrolet</li>
+        <li>Honda</li>
+      </ul>
+      <p>If you have a problem we will always try to fix it. But the most common issues we deal with 
+        include:
+      </p>
+      <ul>
+        <li>Transmissions</li>
+        <li>Brakes</li>
+        <li>Tires</li>
+      </ul>
+    </div>
   )
 }
 
