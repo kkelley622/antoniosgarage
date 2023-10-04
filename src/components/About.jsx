7cbdc3cad0fb7c7bb4107acd56116348ee49f711
Kevin Kelley <kkelley622@gmail.com>
2023-10-04 16:28:03 -0500

    Added paragraphs to About page.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,10 @@ const About = () => {
   return (
     <div>
       <h1>Quien Nostros Somos</h1>
+      <p>Antonio has been in the automotive industry for 30 years. He started his career working at
+        Nissan. He owns and operates this shop since 2014.
+      </p>
+      <p>Chuy also started his career at Nissan.</p>
     </div>
   )
 }
